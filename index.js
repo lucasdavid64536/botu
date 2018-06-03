8cbf7fbc84221f791f80e3718a38739d5420be5b
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2018-06-03 14:27:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bot = new Discord.Client({disableEveryone: true})
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setGame("într-un server foarte bine configurat");
+  bot.user.setGame("Last Night");
 });
 
 bot.login(botconfig.token);
