57944610c2ecf7482e017d6f1231fd54aacf50ab
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2018-06-03 14:34:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const Discord = require("discord.js");
 
 const bot = new Discord.Client({disableEveryone: true})
 
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
-  bot.user.setGame("Last Night");
-});
+client.on('ready', () => {
+  client.user.setGame('Type || ^^help ', 'https://www.twitch.tv/streamerhouse%27)
+})
 
 bot.login(botconfig.token);
