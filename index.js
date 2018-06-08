6e74a08749c0496eb28bfa6582418ce95cf136f2
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2018-06-08 16:30:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,8 @@
 const Discord = require('discord.js');
-const superagent = require("superagent");
-const Client = new Discord.Client();
+const client = new Discord.Client();
 
-const prefix = "!"
-
-
-
-Client.on("ready", () => {
-	console.log("online");
-	Client.user.setPresence({ game: { name: `Hello world`, type: 0} });
-});
-
-// welcome message
-
-Client.on("guildMemberAdd", member => {
-   member.guild.defaultChannel.send("Welcome to: " + member.guild.name + " Hope you enjoy it here")
-});
-
-Client.on("guildMemberRemove", member => {
-   member.guild.defaultChannel.send("Goodbye: " + member.user.username + " from " + member.guild.name)
-});
-
-Client.on("guildCreate", guild => {
-	console.log("Some one added the test bot to a server created by: " + guild.owner.user.username)
-});
-
-Client.on("message", async (message) => {
-	if (message.author.bot) return;
-	if (!message.content.startsWith(prefix)) return;
-	
-	let command = message.content.split(" ")[0];
-	command = command.slice(prefix.length);
-	
-	let args = message.content.split(" ").slice(1);
-	
-	if (command === "ping") {
-		message.channel.send(`Pong! Time took: ${Date.now() - message.createdTimestamp} ms`);
-	} else
-
-	if (command === "say") {
-		message.delete()
-        const embed = new Discord.RichEmbed()
-		.setColor(0x954D23)
-		.setDescription(message.author.username + " says: " + args.join(" "));
-		message.channel.send({embed})
-	} else
-
-	if (command == "help") {
-		const embed = new Discord.RichEmbed()
-		.setColor(0x954D23)
-		.setTitle("Command List:")
-		.addField("!help", "Will give the current command list")
-		.addField("!ping", "WIll show the ping time for the bot")
-		.addField("!say [text]", "Will make the bot say something")
-		.addField("!announcement [text]", "Will make the bot say an announcement and tag everyone")
-		.addField("!cat", "Will send a random cat image");
-		message.channel.send({embed})
-	}
-
-});
-
+client.on('ready', () => {
+  client.user.setGame('tutorial ', 'https://www.twitch.tv/lucasdavid913')
+})
 
 client.login("NDU0MzIwMjQyOTMyNTgwMzYz.Dfrucw.K3FNwekwZ6FQn-ypPR0kz-CCA4Y");
