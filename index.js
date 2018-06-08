aabf11497a5381c09a94db8deb47c8fcbcf67493
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2018-06-08 16:32:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,18 @@ client.on('ready', () => {
   client.user.setGame('tutorial ', 'https://www.twitch.tv/lucasdavid913')
 })
 
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'general');
+    let memberavatar = member.user.avatarURL
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setColor('#FF000')
+        .setThumbnail(memberavatar)
+        .addField('✘ | Nume: ', `${member}`)
+        .addField('✘ | Bun Venit !', `Bun Venit Pe Server Sedare Placuta, ${member}`)
+        .setTimestamp()
+
+        channel.sendEmbed(embed);
+});
+
 client.login("NDU0MzIwMjQyOTMyNTgwMzYz.Dfrucw.K3FNwekwZ6FQn-ypPR0kz-CCA4Y");
