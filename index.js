ac17544eee25e791141c41f9dbc535b61654d561
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2019-05-30 21:37:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  client.user.setGame('tutorial ', 'https://www.twitch.tv/lucasdavid913')
 })
 
 client.on('guildMemberAdd', member => {
@@ -12,11 +11,24 @@ client.on('guildMemberAdd', member => {
         let embed = new Discord.RichEmbed()
         .setColor('#FF000')
         .setThumbnail(memberavatar)
-        .addField('✘ | Nume: ', `${member}`)
-        .addField('✘ | Bun Venit !', `Speram ca esti abont pe canal daca nu #anunturi , ${member}`)
+        .addField('✘ | Name: ', `${member}`)
+        .addField('✘ | Hello !', `${member}`)
+        .setTimestamp()
+
+        channel.sendEmbed(embed);
+});
+client.on('guildMemberLeave', member => {
+    let channel = member.guild.channels.find('name', 'welcome');
+    let memberavatar = member.user.avatarURL
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setColor('#FF0000')
+        .setThumbnail(memberavatar)
+        .addField('✘ | Name: ', `${member}`)
+        .addField('✘ | Hello !', `${member}`)
         .setTimestamp()
 
         channel.sendEmbed(embed);
 });
 
-client.login("NDU0MzIwMjQyOTMyNTgwMzYz.Dfrucw.K3FNwekwZ6FQn-ypPR0kz-CCA4Y");
+client.login("NTgzNzIzMTkzMjQ0NzEyOTcx.XPAhMw.kZbk6cq7O8hB6VWwwaqo3BFznDk");
